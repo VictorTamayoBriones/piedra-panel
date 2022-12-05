a65c899e5977afac6505ba3ca90ff6ff20365fab
Víctor Manuel Tamayo Briones <victortamayo509@gmail.com>
2022-12-04 22:43:03 -0600

    ng add pwa test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -25,6 +25,8 @@ import { UsersCreateComponent } from './components/users-create/users-create.com
 import { AddTagModalComponent } from './components/add-tag-modal/add-tag-modal.component';
 import { DishesFormComponent } from './components/dishes-form/dishes-form.component';
 import { ContactsComponent } from './pages/contacts/contacts.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+
 
 
 @NgModule({
@@ -46,6 +48,7 @@ import { ContactsComponent } from './pages/contacts/contacts.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
     MaterialModule,
     AngularFireModule.initializeApp(environment.firebase),
     AngularFireAuthModule,
